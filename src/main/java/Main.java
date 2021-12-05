1d238899ab33b2663effdb05830cfe797c2ebec1
Zhenya2809 <Zhenya2809@ukr.net>
2021-12-05 22:40:29 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        MyStack stack =new MyStack(5);
+        MyStack stack = new MyStack(5);
         stack.addElement(5);
         stack.addElement(1);
         stack.addElement(3);
         stack.deleteElement();
         System.out.println("__________________________________");
-        System.out.println("top element is "+stack.readTop());
+        System.out.println("top element is " + stack.readTop());
         System.out.println("__________________________________");
-        System.out.println("stack is full? "+stack.isFull());
+        System.out.println("stack is full? " + stack.isFull());
         System.out.println("__________________________________");
-        System.out.println("stack is empty? "+stack.isEmpty());
+        System.out.println("stack is empty? " + stack.isEmpty());
         System.out.println("__________________________________");
-        System.out.println("size of stack = "+stack.getSize());
+        System.out.println("size of stack = " + stack.getSize());
     }
 }
 
--- src/main/java/Stackable.java
+++ src/main/java/Stackable.java
@@ -1,7 +1,11 @@
 public interface Stackable {
-     void addElement(int element);
-     void deleteElement();
-     boolean isEmpty();
-     int readTop();
-     boolean isFull();
+    void addElement(int element);
+
+    void deleteElement();
+
+    boolean isEmpty();
+
+    int readTop();
+
+    boolean isFull();
 }
